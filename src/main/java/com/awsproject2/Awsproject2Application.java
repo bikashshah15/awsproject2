292c8dd7821b88574bdebd74efb8765b6e33ce64
Bikash Shah <bikashshah15@gmail.com>
2024-06-20 13:23:15 -0500

    added project

---
+++ src/main/java/com/awsproject2/Awsproject2Application.java
@@ -0,0 +1,13 @@
+package com.awsproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Awsproject2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Awsproject2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/awsproject2/Awsproject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awsproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Awsproject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
